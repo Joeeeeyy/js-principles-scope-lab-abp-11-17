803512035e3abcbcd22ba9cabb9e6acc3cd2476c
Jose Lopez <jooooeyla@outlook.com>
2019-07-04 16:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Write your solution in this file!
 
 var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = "not bob";
+}
+  
